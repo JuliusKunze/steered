6a48dff10807e7021cb6d1866ec96fe302f8c1e1
Julius Kunze <juliuskunze@gmail.com>
2017-07-04 18:20:36 +0200

    Added possibility for arbitrary mutual information distribution function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from math import log2, sqrt, log
+from math import log2, sqrt, log, exp
 from typing import Iterable, List, Optional
 
 import matplotlib.pyplot as plt
@@ -26,9 +26,9 @@ target_vs_noise_for_partial_cause_feature = inversefunc(mutual_information_of_pa
                                                         image=[0, 1])
 
 
-def test(relevant_feature_count=10, total_feature_count=200, iterations=500):
+def test(relevant_feature_count=10, total_feature_count=200, iterations=1000):
     data = test_dataset(relevant_feature_count=relevant_feature_count,
-                        noise_feature_count=total_feature_count - relevant_feature_count)
+                        total_feature_count=total_feature_count)
     features_with_target = data.columns.values  # type:List[str]
     features = list(filter(lambda i: i != 'target', features_with_target))
     shuffle(features)
@@ -58,7 +58,7 @@ def test(relevant_feature_count=10, total_feature_count=200, iterations=500):
                 count = index + 1
                 yield sum / count
 
-    def run_hics(steered: Optional[bool] = True, steered_exploration_weight=1) -> List[float]:
+    def run_hics(steered: Optional[bool] = True, steered_exploration_weight=.3) -> List[float]:
         mutual_by_feature = dict([(feature, ValuesWithAverage()) for feature in features])
 
         chosen_relevant_feature_counts = []
@@ -91,8 +91,15 @@ def test(relevant_feature_count=10, total_feature_count=200, iterations=500):
     plt.show()
 
 
-def test_dataset(relevant_feature_count: int, noise_feature_count: int, dataset_size=10000,
-                 mutual_information=.5, slight_mutual_information=.45, noise_mutual_information=0):
+def test_dataset(relevant_feature_count: int, total_feature_count: int, dataset_size=10000,
+                 mutual_information=.7, slight_mutual_information=.6, noise_mutual_information=0):
+    def exponential_distribution() -> List[float]:
+        return [((1 - x / 10) if x < .3 else exp(-10 * x)) for x in numpy.linspace(0, 1, num=total_feature_count)]
+
+    distribution = exponential_distribution()
+    plt.plot(distribution)
+    plt.show()
+
     def combination_count(bits: int) -> int:
         return 1 << bits
 
@@ -112,16 +119,9 @@ def test_dataset(relevant_feature_count: int, noise_feature_count: int, dataset_
         return joined_with_noise(random_merged(target, random_values(bits=1), target_share), noise_bits=3)
 
     features = {'target': target}
-    for i in range(int(noise_feature_count * 8 / 10)):
-        features[f"noise-{i}"] = feature(noise_mutual_information)
 
-    for i in range(int(noise_feature_count * 2 / 10)):
-        features[f"noise2-{i}"] = feature(slight_mutual_information)
-
-    target_share = target_vs_noise_for_partial_cause_feature(mutual_information)
-    for i in range(relevant_feature_count):
-        features[f"mutual-{i}"] = joined_with_noise(random_merged(target, random_values(bits=1), target_share),
-                                                    noise_bits=3)
+    for i in range(total_feature_count):
+        features[f"{'mutual' if i < relevant_feature_count else 'noise'}-{i}"] = feature(distribution[i])
 
     return pandas.DataFrame(features)
 
