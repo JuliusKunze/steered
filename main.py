9f71e8493df1d04f6d46b55845e293ee0df35950
Julius Kunze <juliuskunze@gmail.com>
2017-07-05 13:57:28 +0200

    Added standard deviation to plot, adapted parameters to make steering work

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from math import log2, sqrt, log, exp
-from typing import Iterable, List, Optional
+from pathlib import Path
+from random import choice
+from time import strftime
+from typing import List, Optional
 
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import pandas
 from hics.contrast_measure import HiCS
 from numpy import ndarray
-from numpy.random import randint, rand, shuffle, random
-from random import choice
+from numpy.random import randint, rand, shuffle
 from pynverse import inversefunc
 
 
@@ -26,9 +28,14 @@ target_vs_noise_for_partial_cause_feature = inversefunc(mutual_information_of_pa
                                                         image=[0, 1])
 
 
-def test(relevant_feature_count=10, total_feature_count=200, iterations=1000):
-    data = test_dataset(relevant_feature_count=relevant_feature_count,
-                        total_feature_count=total_feature_count)
+def timestamp() -> str:
+    return strftime('%Y%m%d-%H%M%S')
+
+
+def test(relevant_feature_count=10, total_feature_count=200, iterations=1000, runs=10, steered_exploration_weight=1):
+    correlation_distribution = plateau_distribution(total_feature_count)
+    data = generate_data(relevant_feature_count=relevant_feature_count,
+                         correlation_distribution=correlation_distribution)
     features_with_target = data.columns.values  # type:List[str]
     features = list(filter(lambda i: i != 'target', features_with_target))
     shuffle(features)
@@ -46,19 +53,12 @@ def test(relevant_feature_count=10, total_feature_count=200, iterations=1000):
         @property
         def average(self):
             l = len(self.values)
-            if (l == 0):
+            if l == 0:
                 return 0
 
             return self.sum / l
 
-        def partial_averages(self) -> Iterable[float]:
-            sum = 0
-            for index, value in enumerate(self.values):
-                sum += value
-                count = index + 1
-                yield sum / count
-
-    def run_hics(steered: Optional[bool] = True, steered_exploration_weight=.3) -> List[float]:
+    def run_hics(steered: Optional[bool] = True, steered_exploration_weight=steered_exploration_weight) -> List[float]:
         mutual_by_feature = dict([(feature, ValuesWithAverage()) for feature in features])
 
         chosen_relevant_feature_counts = []
@@ -76,7 +76,8 @@ def test(relevant_feature_count=10, total_feature_count=200, iterations=1000):
             feature, value = max(items, key=priority) if steered is not None else choice(items)
 
             print(
-                f"Iteration {iteration}, chosen relevant features: {chosen_relevant_feature_count}, average {value.average:0.3f} in priority {priority((feature, value)):0.3f}.")
+                f"Iteration {iteration}, chosen relevant features: {chosen_relevant_feature_count}, "
+                f"average {value.average:0.3f} in priority {priority((feature, value)):0.3f}.")
 
             mutual_by_feature[feature].append(hics.calculate_contrast([feature], 'target'))
 
@@ -84,21 +85,41 @@ def test(relevant_feature_count=10, total_feature_count=200, iterations=1000):
 
     plt.ylabel("chosen relevant features")
     plt.xlabel("iteration")
+
     # plt.plot(run_hics(steered=None), label="Random")
-    plt.plot(run_hics(steered=True), label="Steered")
-    plt.plot(run_hics(steered=False), label="Flat")
+
+    def plot_average_and_deviation_by_time(steered: bool):
+        color = "b" if steered else "r"
+        label = "Steered" if steered else "Flat"
+
+        mutual_information_by_run_by_time = np.array([run_hics(steered=steered) for _ in range(runs)])
+        average = np.average(mutual_information_by_run_by_time, axis=0)
+        deviation = np.std(mutual_information_by_run_by_time, axis=0)
+
+        plt.fill_between(range(len(average)),
+                         list(average - deviation),
+                         list(average + deviation),
+                         color=color, alpha=.3)
+        plt.plot(average, label=label, color=color)
+
+    plot_average_and_deviation_by_time(steered=True)
+    plot_average_and_deviation_by_time(steered=False)
+
     plt.legend()
+
+    directory = Path(".") / "plots"
+    directory.mkdir(exist_ok=True)
+    fig = plt.gcf()
+    fig.savefig(str(directory / (timestamp() + ".png")))
+
     plt.show()
 
 
-def test_dataset(relevant_feature_count: int, total_feature_count: int, dataset_size=10000,
-                 mutual_information=.7, slight_mutual_information=.6, noise_mutual_information=0):
-    def exponential_distribution() -> List[float]:
-        return [((1 - x / 10) if x < .3 else exp(-10 * x)) for x in numpy.linspace(0, 1, num=total_feature_count)]
-
-    distribution = exponential_distribution()
-    plt.plot(distribution)
-    plt.show()
+def generate_data(relevant_feature_count: int, correlation_distribution: List[float], dataset_size=10000,
+                  show_distribution: bool = False):
+    if show_distribution:
+        plt.plot(correlation_distribution)
+        plt.show()
 
     def combination_count(bits: int) -> int:
         return 1 << bits
@@ -107,7 +128,7 @@ def test_dataset(relevant_feature_count: int, total_feature_count: int, dataset_
         return randint(0, combination_count(bits=bits), count)
 
     def random_merged(values1: ndarray, values2: ndarray, values1_share: float = .5):
-        return numpy.where(rand(len(values1)) < values1_share, values1, values2)
+        return np.where(rand(len(values1)) < values1_share, values1, values2)
 
     def joined_with_noise(values: ndarray, noise_bits: int):
         return [f"{value},{extra_noise}" for value, extra_noise in zip(values, random_values(bits=noise_bits))]
@@ -120,10 +141,19 @@ def test_dataset(relevant_feature_count: int, total_feature_count: int, dataset_
 
     features = {'target': target}
 
-    for i in range(total_feature_count):
-        features[f"{'mutual' if i < relevant_feature_count else 'noise'}-{i}"] = feature(distribution[i])
+    for i in range(len(correlation_distribution)):
+        features[f"{'mutual' if i < relevant_feature_count else 'noise'}-{i}"] = feature(
+            correlation_distribution[i])
 
     return pandas.DataFrame(features)
+
+
+def plateau_distribution(total_feature_count: int) -> List[float]:
+    return [((1 - x / 10) if x < .3 else exp(-10 * x)) for x in np.linspace(0, 1, num=total_feature_count)]
+
+
+def two_relevant_features_distribution(total_feature_count: int) -> List[float]:
+    return [1, 0.95] + ([0] * (total_feature_count - 2))
 
 
 test()
